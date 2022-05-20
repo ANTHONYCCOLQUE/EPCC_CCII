dfe15e23dd71519ff9aa39990c7f91fa1877273b
ANTHONYCCOLQUE <104391632+ANTHONYCCOLQUE@users.noreply.github.com>
2022-05-20 00:46:48 -0500

    Add files via upload

---
+++ sem03/multiplicationTable.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int m[20][20];
+	
+	
+	cout<<"*|\t";
+	for ( int i=1; i<=10 ; i++){
+		cout<<i<<"\t";
+		
+	}
+	cout<<"\n----------------------------------------------------------------------------------\n";
+	for ( int i=1,a=1 ; i<=10; i++, a++ ){
+		cout<<a<<"|\t";
+		for ( int j=1; j<=10; j++){
+			
+			m[i][j] = i*j;
+			cout<<m[i][j]<<"\t";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
